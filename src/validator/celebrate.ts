cc4e9315a3cd1be967d19fc9e8150d8557ac3662
vio-ubuntu <saiviolet@gmail.com>
2023-03-26 20:13:31 +0300

    ПРАВКИ ПО РЕВЬЮ / все пункты

--- src/validator/celebrate.ts
+++ src/validator/celebrate.ts
@@ -25,8 +25,8 @@ export const validatorUserId = celebrate({
 // 2.2 создание пользователя
 export const validatorCreateUser = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(urlRegExp),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
